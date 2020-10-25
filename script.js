8961ee2937edd7080e3dee92b69bb838e982724f
Harry Hackett <yrrah2@users.noreply.github.com>
2020-10-26 03:54:03 +1300

    Added many test logs

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ const lat = -43.53;
 const long = -172.62;
 
 const s = suntimes(lat, long);
-console.log(s);
+const st = [s[0]+3, s[1]+3];
+console.log("Sunset and sunrise array: " + st);
 
 var d = new Date();
-console.log(d.getHours + ": " + d.getMinutes);
+var hh = d.getHours();
+var mm = d.getMinutes();
+var ss = d.getSeconds();
+
+var time = hh + ( mm + (ss/60) )/60;
+
+console.log("Time Now: " + hh + ": " + mm);
+console.log("Time Now in decimal: " + time);
+
+if (st[1] > time > st[0]){
+  console.log("Hours after sunrise: " + time - st[0]);
+} else if (time > st[1]) {
+  console.log("Hours after sunset: " + time - st[1]);
+} else {
+  console.log("Hours after sunset: " + st[0] - time + 24 - st[1]);
+};
