47f920df709384042c11aeb6da6c2d2e0db24226
Harry Hackett <yrrah2@users.noreply.github.com>
2020-10-26 13:43:29 +1300

    Improved display text section, added window pageload

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-const lat = -43.53;
-const long = -172.62;
-
-const s = suntimes(lat, long);
-const st = [s[0]+3, s[1]+3];
-console.log("Sunset and sunrise array: " + st);
-
-var d = new Date();
-var hh = d.getHours();
-var mm = d.getMinutes();
-var ss = d.getSeconds();
-
-var time = hh + ( mm + (ss/60) )/60;
-
-console.log("Time Now: " + hh + ": " + mm);
-console.log("Time Now in decimal: " + time);
-
-if (st[1] > time > st[0]){
-  hAfter = time - st[0];
-  $("#natural").text("Hours after sunrise: " + hAfter);
-} else if (time > st[1]) {
-  hAfter = time - st[1];
-  $("#natural").text("Hours after sunset: " + hAfter);
-} else {
-  hAfter = st[0] - time + 24 - st[1];
-  $("#natural").text("Hours after sunset: " + hAfter);
+const newTime = (lat, long) => {
+    const s = suntimes(lat, long);
+    const st = [s[0]+3, s[1]+3];
+    console.log("Sunset and sunrise array: " + st);
+    return st;
 };
 
+const displayTime = (sunArray) => {
+    var d = new Date();
+    var hh = d.getHours();
+    var mm = d.getMinutes();
+    var ss = d.getSeconds();
+
+    var time = hh + ( mm + (ss/60) )/60;
+
+    console.log("Time Now: " + hh + ": " + mm);
+    console.log("Time Now in decimal: " + time);
+
+    if (sunArray[1] > time > sunArray[0]){
+        hAfter = time - sunArray[0];
+        displayText = "Hours after sunrise: " + hAfter;
+    } else if (time > sunArray[1]) {
+        hAfter = time - sunArray[1];
+        displayText = "Hours after sunset: " + hAfter;
+    } else {
+        hAfter = sunArray[0] - time + 24 - sunArray[1];
+        displayText = "Hours after sunset: " + hAfte;
+    };
+
+    console.log(displayText);
+    $("#natural").text(displayText);
+}
+
+const pageload = () => {
+    const lat = -43.53;
+    const long = -172.62;
+
+    var sunArray = newTime(lat, long);
+    displayTime(sunArray);
+}
+
+window.onload = pageload;
