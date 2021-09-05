d983cafe17a2805979b80e0a739f3c06d158a24b
Harry Hackett <yrrah2@users.noreply.github.com>
2021-09-05 13:50:09 +1200

    Cleaned up

--- script.js
+++ script.js
@@ -33,65 +33,65 @@ const timeDifference = (time1, time2) => {
 };
 
 const convertTime = (time) => {
-	/*Converts a float time into a readable time.
-	e.g. 17.17 --> 17:10*/
-	var hour = Math.floor(time);
+	/* 
+	* Converts a float time into a readable time.
+	* e.g. 17.17 --> 17:10
+	*/
 	
-	var minute = Math.floor((time - Math.floor(time)) * 60);
+	let hour = Math.floor(time);
+	let minute = Math.floor((time - Math.floor(time)) * 60);
 	
 	return timeFix(hour, minute);
 }
 
-const newTime = (lat, long) => suntimes(lat, long)[0];
+const displayTime = (international_time, sunrise_time, natural_time) => {
+	$("#international").text("International: " + international_time[0] + ':' + international_time[1]);
+	$("#sunrise").text("Sunrise occurs at: " + sunrise_time[0] + ':' + sunrise_time[1]);
+	$("#natural").text("Natural: " + natural_time[0] + ':' + natural_time[1]);
+}
 
-const displayTime = (sunrise) => {
-	var d = new Date();
-	var hh = d.getUTCHours();
-	var mm = d.getUTCMinutes();
-	var ss = d.getUTCSeconds();
+const calculateTimes = (lat, long) => {
 
-	var internationalTimeDifference = convertTime(12*24.950833/180); /*24.95 is the longitude of Lake Makgadikgadi, the origin of the human species. 180 is half the degrees of a circle, and 12 is half the hours of the day.*/
+	let d = new Date();
+	let hour_utc = d.getUTCHours();
+	let minute_utc = d.getUTCMinutes();
+	let second_utc = d.getUTCSeconds();
 	
-	var international_time = timeFix(hh+internationalTimeDifference[0], mm+internationalTimeDifference[1]);
+	let time_now_utc = hour_utc + ( minute_utc + (second_utc/60) )/60;
+	let sunrise_utc = suntimes(lat, long)[0];
 	
-	var time = hh + ( mm + (ss/60) )/60;
-	var [natural_hour, natural_minute] = convertTime(timeDifference(time, sunrise));
-	console.log("Time: "+time);
-	console.log("sunrise: "+sunrise);
-	console.log("timeDifference: "+timeDifference(time, sunrise));
-	console.log("dif converted: "+convertTime(timeDifference(time, sunrise)));
-	console.log("n_hour: "+natural_hour);
+	/*
+	* Find the current time in Lake Makgadikgadi, the origin of the human species.
+	* 24.95 is the longitude, 180 is half the degrees of a circle, and 12 is half the hours of the day.
+	*/
+	let internationalTimeDifference = convertTime(12*24.950833/180); 
+	let international_time = timeFix(hour_utc+internationalTimeDifference[0], minute_utc+internationalTimeDifference[1]);
 	
-	
-	var sunrise_time = convertTime(sunrise);
+	/* Find the time of local sunrise in international time	*/
+	let sunrise_time = convertTime(sunrise_utc);
 	sunrise_time = timeFix(internationalTimeDifference[0]+sunrise_time[0], internationalTimeDifference[1]+sunrise_time[1]);
 	
-	if (time>sunrise) {var n_hour = international_time[0] - sunrise_time[0]; var n_minute = international_time[1] - sunrise_time[1]; console.log("Natural hour: "+n_hour);}
-	else if (time<sunrise) {}
-	else {};
+	/* Find the natural time (time since sunrise) */
+	let natural_time = convertTime(timeDifference(time_now_utc, sunrise_utc));	
 	
-	$("#international").text("International: " + international_time[0] + ':' + international_time[1]);
-	$("#sunrise").text("Sunrise occurs at: " + sunrise_time[0] + ':' + sunrise_time[1] + " (INT)");
-	$("#natural").text("Natural: " + natural_hour + ':' + natural_minute);
-}
+	displayTime(international_time, sunrise_time, natural_time);
+};
 
 const pageload = () => {
 	var lat = -20.716667;	/* Using the coordinates of Lake Makgadikgadi, the origin of the human species. */
 	var long = 24.950833;	
-	displayTime(newTime(lat, -long));
+	calculateTimes(lat, long));
 
 	var locationPromise = getLocation();
 	locationPromise
 		.then((loc) => {
 			[lat, long] = loc;
 		console.log("lat: "+lat+", long: "+long);
-			displayTime(newTime(lat, long));
+			calculateTimes(lat, long));
 		})
 		.catch((err) => { 
 			console.log("No location");
 		});
-
-	
 }
 
 window.onload = pageload;
